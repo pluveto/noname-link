65c84460448ce8cc90e96893efefb6c0e0df9570
pluveto <i@pluvet.com>
2022-12-17 16:18:24 +0800

    feat: initial code base

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# demo1
+# NONAME
+
+## Build
+
+```sh
+make install
+```
+
+## License
+
+NONAME is licensed under the Apache License 2.0
---
+++ app/encoding.go
@@ -0,0 +1,17 @@
+package noname
+
+import (
+	"github.com/pluveto/demo1/v1/app/params"
+
+	"github.com/cosmos/cosmos-sdk/std"
+)
+
+// MakeEncodingConfig creates an EncodingConfig for testing
+func MakeEncodingConfig() params.EncodingConfig {
+	encodingConfig := params.MakeEncodingConfig()
+	std.RegisterLegacyAminoCodec(encodingConfig.Amino)
+	std.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+	ModuleBasics.RegisterLegacyAminoCodec(encodingConfig.Amino)
+	ModuleBasics.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+	return encodingConfig
+}
